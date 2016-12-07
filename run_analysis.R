7153f876b7bdb7a140493626ac68b975f37cca11
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-12-07 23:58:51 +0100

    Rename variables to make it clear. Enhance comments

--- run_analysis.R
+++ run_analysis.R
@@ -4,29 +4,26 @@ run_analysis <- function() {
     #download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", dest="dataset.zip")
     #unzip("dataset.zip")
 
-    # separator is ‘white space’, that is one or more spaces, tabs
+    # NOTE: in header arguments, separator is ‘white space’, that is one or more spaces, tabs
     
-    ## Read what will be become the data header
-    data_header <- read.csv("UCI HAR Dataset/features.txt", sep = "", header = FALSE)[,2]
-
     ## Read the activity labels
     activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
       
-    ## Read and merge test and training data. Add the column names to the data
-    test_data <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
-    train_data <- read.csv("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
-    merged_data <- rbind(test_data, train_data)   
-    names(merged_data) <- data_header
+    ## Read and merge test and training data. Add the column names to the data. Use the features file as its header
+    test_features <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
+    train_features <- read.csv("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
+    features <- rbind(test_features, train_features)   
+    names(features) <- read.csv("UCI HAR Dataset/features.txt", sep = "", header = FALSE)[,2]
       
-    ## Read and merge test and training activity type. Add the column name to the activity types column
-    test_types <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
-    train_types <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
-    merged_types <- rbind(test_types, train_types)
-    names(merged_types) <- "activity"
+    ## Read and merge test and training activity types. Add the column name to the activity types column
+    test_activities <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
+    train_activities <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
+    activities <- rbind(test_activities, train_activities)
+    names(activities) <- "activity"
    
-    ## Reading and merge test and training subject ids. Add the column name to the subject id column
+    ## Read and merge test and training subject ids. Add the column name to the subject id column
     test_subjects <- read.csv("UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
     train_subjects <- read.csv("UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
-    merged_subjects <- rbind(test_subjects, train_subjects)
-    names(merged_subjects) <- "subject_id"
+    subjects <- rbind(test_subjects, train_subjects)
+    names(subjects) <- "subject_id"
 }  
