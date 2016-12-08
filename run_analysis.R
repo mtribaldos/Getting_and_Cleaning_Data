54982efd01e40779bbc09a6ec13e5832b0160456
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-12-08 01:34:45 +0100

    Full generation of the definitive dataset

--- run_analysis.R
+++ run_analysis.R
@@ -6,24 +6,36 @@ run_analysis <- function() {
 
     # NOTE: in header arguments, separator is ‘white space’, that is one or more spaces, tabs
     
-    ## Read the activity labels
-    activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
-      
-    ## Read and merge test and training data. Add the column names to the data. Use the features file as its header
+    ## Read and merge test and training data. 
+    ## Add the column names to the data. Use the features file as its header
+    ## Select only some features based on its name 
     test_features <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
     train_features <- read.csv("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
     features <- rbind(test_features, train_features)   
     names(features) <- read.csv("UCI HAR Dataset/features.txt", sep = "", header = FALSE)[,2]
+    selected_features <- features[, grep("mean..|std..|activityId", names(features))] 
       
-    ## Read and merge test and training activity types. Add the column name to the activity types column
+    ## Read and merge test and training activity types. 
+    ## Add the column name to the activity types column
     test_activities <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
     train_activities <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
     activities <- rbind(test_activities, train_activities)
     names(activities) <- "activity"
    
-    ## Read and merge test and training subject ids. Add the column name to the subject id column
+    ## Read and merge test and training subject ids. 
+    ## Add the column name to the subject id column
     test_subjects <- read.csv("UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
     train_subjects <- read.csv("UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
     subjects <- rbind(test_subjects, train_subjects)
     names(subjects) <- "subject_id"
+
+    ## Read the activity labels
+    ## Substitute activity id for activity type (descriptive)
+    activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
+    names(activity_labels) <- c("activity_id", "activity_type")
+    merge(activities, activity_labels, by.x = "activity", by.y = "activity_id", all.x = TRUE)
+    activities <- activities[, -grep("activity", names(activities))]
+
+    # Generate the definitive dataset
+    dataset <- cbind(subjects, selected_features, activities)
 }  
